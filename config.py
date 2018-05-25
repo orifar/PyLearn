66eb9842096c75a29afd22e8c4b1fc9e4181087f
orifar <rifar317@gmail.com>
2018-05-25 04:55:18 +0000

    twitter search
    twitter search

---
+++ config.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+# --------------------------------------------------------------------------------
+
+consumer_key = "cZ46DlefXHqctz1bsJ0RV2sqL"
+consumer_secret = "xXXXXXXXXxxxxXxXXxxXxxXXxXxXxxxxXxXXxxxXXx"
+access_key = "XXXXXXXX-xxXXxXXxxXxxxXxXXxXxXxXxxxXxxxxXxXXxXxxXX"
+access_secret = "XxXXXXXXXXxxxXXXxXXxXxXxxXXXXXxXxxXXXXx"
